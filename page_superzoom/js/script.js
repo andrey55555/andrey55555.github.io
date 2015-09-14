ddf8c4e8e18ce06e70d3a23aace184ab571b872e
Andrey Maksimovich <andrey.55555@yandex.ru>
2015-09-15 00:44:12 +0300

    superzoom home

--- page_superzoom/js/script.js
+++ page_superzoom/js/script.js
@@ -24,6 +24,8 @@ var common = (function(){
 })();
 
 $(document).ready(function(){
+	common.center('#fvg_wrapper');
+	//$('.fvg_ca_img').zoom({ on:'click' });
 
 	$("#vg_thumbs").on('click','.vg_th_img',function(){
 		var number = $(this).attr('data-item');
@@ -33,22 +35,23 @@ $(document).ready(function(){
 	$("#fvg_thumbs").on('click','.fvg_th_img',function(){
 		var number = $(this).attr('data-item');
 		$("#fvg_wrapper").ecarousel({tagC:".fvg_ca_img",tagI:".fvg_th_img",num:number-1,ind:true});
-		$('.fvg_ca_img[data-item='+number+']').zoom().trigger('click');
+		$('.fvg_ca_img[data-item='+number+']').trigger('zoom.destroy').zoom({ on:'click' }).trigger('click');
+	});
+	
+	$("#fvg_carousel").on('click','.fvg_ca_img',function(){
+		var number = $(this).attr('data-item');
+		console.log(number);
 	});
 
-	common.center('#fvg_wrapper');
-	//$('#fvg_carousel').zoom();
-
-	$('.under-layer').on('click',function(){
+	$('.under-layer,#fvg_wrapper-close').on('click',function(){
 		$('.under-layer').hide();
 		$('#fvg_wrapper').css('visibility','hidden');
 	})
 
 	$('#vg_carousel').on('click','.vg_ca_img',function(){
 		var number = $(this).attr('data-item');
-		$('.fvg_ca_img[data-item='+number+']').zoom().trigger('click');
 		$('.under-layer').show();
 		$('#fvg_wrapper').css('visibility','visible');
-		
+		$('.fvg_th_img[data-item='+number+']').trigger('click');
 	})
 });
